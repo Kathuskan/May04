2717b50f6c109ba32705bb928e75551c57048b2a
kathuskan <kathus2003@gmail.com>
2025-05-09 07:12:18 +0530

    1st gpt

--- server/models/users.js
+++ server/models/users.js
@@ -1,17 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
     const Users = sequelize.define("users", {
-      firstname: {
+      firstName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      lastname: {
+      lastName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
       phone: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
-      }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     });
   
     return Users;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {users} = require('../models')
+const { users } = require('../models');
 
-//routers
+// GET all users
 router.get("/", async (req, res) => {
-    const listOfUsers = await users.findAll();
-    res.json(listOfUsers);
+  const listOfUsers = await users.findAll();
+  res.json(listOfUsers);
 });
 
+// POST new user
 router.post("/", async (req, res) => {
-    
-
-    const user = req.body;
-    await users.create(user);
-    res.json(user);
-})
-
+  try {
+    const { firstName, lastName, phone, email, password } = req.body;
+    const newUser = await users.create({
+      firstName,
+      lastName,
+      phone,
+      email,
+      password,
+    });
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Could not create user", error: err.message });
+  }
+});
 
 module.exports = router;
